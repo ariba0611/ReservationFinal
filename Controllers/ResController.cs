d52840577914448ee7f351246e2dda2a514eb01c
ariba.siddiqui <aribasiddiqui111@gmail.com>
2022-12-17 19:48:47 +0530

    Add project files.

---
+++ Controllers/ResController.cs
@@ -0,0 +1,54 @@
+﻿using ReservationPayment.Models;
+using ReservationPayment.Models.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ReservationPayment.Controllers
+{
+    public class ResController : Controller
+    {
+        private IRailwayRepository<TrainDetails> TrainObj;
+        public ResController()
+        {
+            this.TrainObj = new RailwayRepository<TrainDetails>();
+
+        }
+        public ActionResult Index()
+        {
+
+            List<TrainDetails> trainDetails = TrainObj.GetModel().ToList();
+
+            ViewData["TrainDetailS"] = new SelectList(trainDetails, dataValueField: "SourceStation", dataTextField: "SourceStation");
+            ViewData["TrainDetailDesti"] = new SelectList(trainDetails, dataValueField: "DestinationStation", dataTextField: "DestinationStation");
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Index(Reservation res)
+
+        {
+           
+                Session["ResName"] = res.Res_Name ;
+                Session["ResGender"] = res.Res_Gender;
+            
+                List<TrainDetails> trainDetails = TrainObj.GetModel().ToList();
+
+               ViewData["TrainDetailS"] = new SelectList(trainDetails, dataValueField: "SourceStation", dataTextField:  "SourceStation");
+               ViewData["TrainDetailDesti"] = new SelectList(trainDetails,dataValueField: "DestinationStation",dataTextField: "DestinationStation");
+
+                Session["SourceStation"] = res.TrainDetails.SourceStation;
+
+               Session["DestinationStation"] = res.TrainDetails.DestinationStation;
+
+
+
+
+
+            return View();
+
+        }
+
+    }
+}
